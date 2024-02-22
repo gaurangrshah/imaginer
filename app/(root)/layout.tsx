5113ff31b5c5bf938f436755c08f40469d8262ed
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-21 23:09:30 -0500

    feat: sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,9 @@
+import { Sidebar } from '@/components/shared/sidebar';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="root">
+      <Sidebar />
       <div className="root-container">
         <div className="wrapper">
           {children}
