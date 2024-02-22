3c739dfb119d319942bd31cbcf27dca5597a368e
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-21 23:49:54 -0500

    chore: add mongodb + configure mongoose

---
+++ lib/db/mongoose.ts
@@ -0,0 +1,40 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const MONGODB_URL = process.env.MONGODB_URL!;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+// This function is responsible for connecting to the database
+export const connectToDatabase = async () => {
+  // If there is already a connection, return it
+  if (cached.conn) return cached.conn;
+
+  // If the MONGODB_URL is not provided, throw an error
+  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  // If there is no pending promise to connect, create one and connect to the database
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: 'imaginify',
+      bufferCommands: false,
+    });
+
+  // Wait for the connection promise to resolve and assign the connection to the cached.conn variable
+  cached.conn = await cached.promise;
+
+  // Return the connection
+  return cached.conn;
+};
