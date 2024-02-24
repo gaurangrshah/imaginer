9a880f85080f7cc1fb914625bc40d29724e95eb1
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-23 23:07:12 -0500

    feat: implement cloudinary AI image utils + views

--- app/(root)/transformations/[id]/add/[type]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AddTransformationTypePage() {
-  return (
-    <>
-      Home
-    </>
-  );
-}
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,29 @@
+import { redirect } from 'next/navigation';
+
+import { auth } from '@clerk/nextjs';
+
+import { Header } from '@/components/shared/header';
+import { TransformationForm } from '@/components/shared/transformation-form';
+
+import { transformationTypes } from '@/constants';
+import { getUserById } from '@/lib/actions/user.actions';
+
+export default async function AddTransformationTypePage({ params: { type } }: SearchParamProps) {
+  const transformation = transformationTypes[type];
+  const { userId } = auth()
+  if (!userId) return redirect('/sign-in')
+  const user = await getUserById(userId)
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          type={transformation.type as TransformationTypeKey}
+          userId={user._id}
+          creditBalance={user?.creditBalance}
+        />
+      </section>
+    </>
+  );
+}
